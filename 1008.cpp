46cfc322378917674465bd4e52983074fd38ab24
minjae-lulu <79103625+minjae-lulu@users.noreply.github.com>
2021-07-05 15:36:42 +0900

    update

---
+++ 1008.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() 
+{ 
+    int a;
+    int b;
+    cin>>a >> b;
+
+    cout.precision(10);
+    cout<< static_cast<double>(a)/static_cast<double>(b);
+
+    return 0;
+}
