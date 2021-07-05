f75b00a6d1c1955bec36393fdcdeb95ef8417af2
minjae-lulu <79103625+minjae-lulu@users.noreply.github.com>
2021-07-05 15:54:01 +0900

    update

---
+++ 9498.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+using namespace std;
+
+
+int main() 
+{ 
+
+int A;
+cin>>A;
+
+if (A<=100 && A>=90){
+    cout<< "A"; 
+}
+else if (A<=89 && A>=80){
+    cout<< "B"; 
+}
+else if (A<=79 && A>=70){
+    cout<< "C"; 
+}
+else if (A<=69 && A>=60){
+    cout<< "D"; 
+}
+else{
+    cout << "F";
+}
+
+return 0;
+}
