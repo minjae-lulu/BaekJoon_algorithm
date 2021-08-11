5239002d31c1fa330d0f1611cae79a62e9b01569
minjae-lulu <lmjbsj@unist.ac.kr>
2021-08-11 12:46:50 +0900

    update

--- 12865.cpp
+++ 12865.cpp
@@ -2,9 +2,8 @@
 #include <algorithm>
 using namespace std; // using dp, this problem called 01 KnapSack
 
-int n,k;
-int item[102][2]={0,}, D[102][1000001]={0,};
-
+    int n,k;
+    int item[102][2]={0,}, D[102][1000001]={0,};
 int main(){
     cin.tie(0);
     cin.sync_with_stdio(0);
@@ -15,11 +14,11 @@ int main(){
         cin >> item[i][0] >> item[i][1]; // [i][0] = weight, [i][1] = worth
     }
 
-    for(size_t i=1; i<=n; i++){
+    for(size_t i=1; i<=n; i++){ // i is number of item
         // not get : D[i][j] = D[i-1][j];
         // get : D[i][j] = D[i][j - item_weight] + item_worth
-        for(size_t j=0; j<=k; j++){
-            if(j<item[i][0])    D[i][j] = D[i-1][j];
+        for(size_t j=0; j<=k; j++){ // j is max weight of item
+            if(j<item[i][0])    D[i][j] = D[i-1][j]; // can't get
             else D[i][j] = max(D[i-1][j], D[i-1][j-item[i][0]]+item[i][1]);
         }
     }
