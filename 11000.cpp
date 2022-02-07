20688e4493c834a270ec7ada9f5abb600e6b7351
minjae-lulu <lmjbsj@unist.ac.kr>
2022-02-07 17:01:39 +0900

    update

---
+++ 11000.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <algorithm>
+#include <string>
+using namespace std; 
+
+int main(){
+    ios::sync_with_stdio(false);
+    cin.tie(NULL),cout.tie(NULL);
+    
+    int n; cin >> n;
+    int st,end;
+    for(int i=0; i<n; i++){
+        cin >> st >> end;
+        
+    }
+
+    return 0;
+}
