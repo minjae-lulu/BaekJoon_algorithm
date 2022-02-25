9b5ec132097e64db4cd2080074f8de48df346698
minjae-lulu <lmjbsj@unist.ac.kr>
2022-02-25 19:54:40 +0900

    update

--- 11286.cpp
+++ 11286.cpp
@@ -8,7 +8,7 @@ using namespace std;
 class cmp{ // pq는 bool 함수가 아닌 class 로 cmp를 구현해야 한다. pq는 컨테이너이기 때문.
     public:
     bool operator() (int a, int b){
-        if(abs(a) != abs(b))    return abs(a) > abs(b); // 앞의 abs가  크면 true
+        if(abs(a) != abs(b))    return abs(a) > abs(b); // 앞의 abs가  크면 true // 절댓값 작은것이 왼쪽으로 정렬하겠다
         return a>0 && b <0; // 절댓값이 같은 경우는 a가 양수고 b가 음수일때만 True 아니면 False
     }
 };
