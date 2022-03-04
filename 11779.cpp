c7f0ab9375ffe6b692a2237f4cdc53c38040be0f
minjae-lulu <lmjbsj@unist.ac.kr>
2022-03-04 20:51:05 +0900

    update

--- 11779.cpp
+++ 11779.cpp
@@ -1,25 +1,26 @@
 #include <iostream>
 #include <vector>
 #include <queue>
-// v랑 V랑 헷갈려서 이상하게 됨.... 여러분들은 그런실수 하지 말길...
+#include <algorithm>
 using namespace std;
 
-vector<pair<int,int> > adj[20005]; // {1,2} {3,1} {4,2} ... 이런식으로 하겠다
-int d[20005];
-int V,E,st;
+vector<pair<int,int> > adj[1005]; // {1,2} {3,1} {4,2} ... 이런식으로 하겠다
+int d[1005];
+int path[1005];
+int V,E,st,en;
 
 int main(){
     cin.tie(0);
     cin.sync_with_stdio(0);
     
     cin >> V >> E;
-    cin >> st;
     int u,v,w;
     fill(d,d+V+1,100000000); // 우선 거리를 무한으로 둠
     for(int i=0; i<E; i++){
         cin >> u >> v >> w;
         adj[u].push_back(make_pair(w,v)); // 가중치 우선정렬 할것이기 때문에
     }
+    cin >> st >> en;
     priority_queue<pair<int,int>, vector<pair<int,int> >, greater<pair<int,int> > > pq; // 최소힙 {가중치, 다음노드}
     d[st]=0; 
 
@@ -31,13 +32,21 @@ int main(){
             if(d[nxt.second] <= d[cur.second]+ nxt.first)   continue; //조건안만족하면(갱신필요 X) for문 다시돔
             d[nxt.second] = d[cur.second]+ nxt.first;
             pq.push(make_pair(d[nxt.second],nxt.second));
+            path[nxt.second]=cur.second; // 어디로 부터 왔는지 경로 저장 (경로를 저장하는 arr)
             
         }
     }
 
-    for(int i=1; i<=V; i++){
-        if(d[i] == 100000000) cout << "INF" << '\n';
-        else            cout << d[i] << '\n';
+    cout << d[en] << '\n'; // 도착지점까지 최단거리
+    vector<int> ans;
+    int flow = en; // 끝부터 추가해가면서 vec에 값저장.
+    while(flow != st){ // 시작점에서 만나면 끝
+        ans.push_back(flow);
+        flow = path[flow];
     }
+    ans.push_back(flow); // 마지막 시작지점도 추가
+    reverse(ans.begin(), ans.end());
+    cout << ans.size() << '\n';
+    for(auto i : ans)   cout << i << " ";
 
 }
